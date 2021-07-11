9d7501988f3967e8960c328aa008c056c0b17c04
kaetaen <kaetaen@tutanota.com>
2021-07-11 00:17:13 -0300

    create Receptor class

--- zagred/main.py
+++ zagred/main.py
@@ -1,30 +1,37 @@
 import speech_recognition
 import pyttsx3
 
-def get_audio():
-    microphone = speech_recognition.Recognizer()
+class Receptor:
+    def listen(self):
+        microphone = speech_recognition.Recognizer()
+        
+        with speech_recognition.Microphone() as source:
+            microphone.adjust_for_ambient_noise(source)
+            audio = microphone.listen(source)
+        
+        try:
+            print("Ouvindo... ")
+            phrase = microphone.recognize_google(audio,language='pt-BR')
+            self.__speak("Você disse: " + phrase)
+        except: # speech_recognition.UnknownValueError
+            self.__speak("Não entendi")
+        
+        return phrase
 
-    with speech_recognition.Microphone() as source:
-        microphone.adjust_for_ambient_noise(source)
-        print("Diga alguma coisa: ")
-        audio = microphone.listen(source)
-    
-    try:
-        phrase = microphone.recognize_google(audio,language='pt-BR')
-        print("Você disse: " + phrase)
-    except speech_recognition.UnkownValueError:
-        print("Não entendi")
-    
-    return frase
+
+    def __speak(self, phrase):
+        speaker = pyttsx3.init()
+        voices = speaker.getProperty('voices')
+        speaker.setProperty('voice', voices[53].id)
+        rate = speaker.getProperty('rate')
+        speaker.setProperty('rate', rate-80)
+        speaker.say(phrase)
+        speaker.runAndWait()
 
 
-def talk(phrase):
-    speaker = pyttsx3.init()
-    voices = speaker.getProperty('voices')
-    speaker.setProperty('voice', voices[53].id)
-    rate = speaker.getProperty('rate')
-    speaker.setProperty('rate', rate-80)
-    speaker.say(phrase)
-    speaker.runAndWait()
+if __name__ == '__main__':
+    zagred = Receptor()
+    zagred.listen()
+
 
 
